91c3fb6cb7cc0ee7fe419650f97069d9ca3a482e
SobolMaria <manya.sobol@gmail.com>
2024-07-16 21:26:39 +0300

    in_func

--- task_1.py
+++ task_1.py
@@ -2,19 +2,20 @@ from datetime import datetime
 
 
 def get_days_from_today(your_date):
+    while True:
+        try:
+            datetime_date = datetime.strptime(your_date, "%Y-%m-%d")
+            if datetime_date:
+                break
+        except:
+            print(f"{your_date} has invalid format, please enter date in format 'РРРР-ММ-ДД': ")
+            your_date = input("Enter data in format 'РРРР-ММ-ДД': ")
     datetime_date = datetime.strptime(your_date, "%Y-%m-%d").date()
     today = datetime.today().date()
     days_from_today = (datetime_date - today).days
 
     return days_from_today
 
-while True:
-    your_date = input("Enter data in format 'РРРР-ММ-ДД': ")
-    try:
-        datetime_date = datetime.strptime(your_date, "%Y-%m-%d")
-        if datetime_date:
-            break
-    except:
-        print(f"{your_date} has invalid format, please enter date in format 'РРРР-ММ-ДД': ")
 
+your_date = input("Enter data in format 'РРРР-ММ-ДД': ")
 print(get_days_from_today(your_date), " DAYS") 
--- task_2.py
+++ task_2.py
@@ -1,8 +1,41 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    set_of_numbers = set()
-    number = 0
+    while True:
+        try:
+            min = int(min)
+            if min >= 1 or min >= 1000:
+                break
+            else:
+                print("Enter minimum >= 1")
+        except ValueError:
+            print("Enter a number minimum!")
+            min = input("Enter minimum: ")
+
+    while True:
+        try:
+            max = int(max)
+            if  min <= max <= 1000:
+                break
+            else:
+                print(f"Enter number {min} <= number <= 1000")
+        except ValueError:
+            print("Enter a number maximum!")
+            max = input("Enter maximum: ")
+
+    while True:
+        try:
+            quantity = int(quantity)
+            if  max - min >= quantity:
+                break
+            else:
+                max_qty = max - min
+                print(f"Enter number <= {max_qty}")
+        except ValueError:
+            print("Enter a number between min and max!")
+            quantity = input("Enter quantity between min and max: ")
+        set_of_numbers = set()
+        
     while len(set_of_numbers) < quantity:
         num = random.randint(min, max)
         set_of_numbers.add(num)
@@ -10,38 +43,10 @@ def get_numbers_ticket(min, max, quantity):
     list_of_numbers = list(set_of_numbers)
     return list_of_numbers
         
-while True:
-    minimum = input("Enter minimum: ")
-    try:
-        minimum = int(minimum)
-        if minimum >= 1 or minimum >= 1000:
-            break
-        else:
-            print("Enter minimum >= 1")
-    except ValueError:
-        print("Enter a number!")
 
-while True:
-    maximum = input("Enter maximum: ")
-    try:
-        maximum = int(maximum)
-        if minimum <= maximum <= 1000:
-            break
-        else:
-            print(f"Enter number {minimum} <= number <= 1000")
-    except ValueError:
-        print("Enter a number!")
 
-while True:
-    quantity = input("Enter quantity between min and max: ")
-    try:
-        quantity = int(quantity)
-        if  maximum - minimum >= quantity:
-            break
-        else:
-            max_qty = maximum - minimum
-            print(f"Enter number <= {max_qty}")
-    except ValueError:
-        print("Enter a number!")
 
+minimum = input("Enter minimum: ")
+maximum = input("Enter maximum: ")
+quantity = input("Enter quantity between min and max: ")
 print(get_numbers_ticket(minimum, maximum, quantity))
