09db00ceb54a908ba9a9f7d86c1f2c3884fa8ca0
SobolMaria <manya.sobol@gmail.com>
2024-07-21 01:13:34 +0300

    updated

--- task_2.py
+++ task_2.py
@@ -1,40 +1,19 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    while True:
-        try:
-            min = int(min)
-            if min >= 1 or min >= 1000:
-                break
-            else:
-                print("Enter minimum >= 1")
-        except ValueError:
-            print("Enter a number minimum!")
-            min = input("Enter minimum: ")
+    try:
+        min = int(min)
+        max = int(max)
+        quantity = int(quantity)
+    except ValueError:
+        print("Unacceptable numbers!")
+        return []
 
-    while True:
-        try:
-            max = int(max)
-            if  min <= max <= 1000:
-                break
-            else:
-                print(f"Enter number {min} <= number <= 1000")
-        except ValueError:
-            print("Enter a number maximum!")
-            max = input("Enter maximum: ")
-
-    while True:
-        try:
-            quantity = int(quantity)
-            if  max - min >= quantity:
-                break
-            else:
-                max_qty = max - min
-                print(f"Enter number <= {max_qty}")
-        except ValueError:
-            print("Enter a number between min and max!")
-            quantity = input("Enter quantity between min and max: ")
-        set_of_numbers = set()
+    if min < 1 or max > 1000 or quantity > max - min:
+        print("Unacceptable data")
+        return []
+    
+    set_of_numbers = set()
         
     while len(set_of_numbers) < quantity:
         num = random.randint(min, max)
